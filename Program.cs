bd6194f45c52731210e2a8d4c66ac50aa5470fe7
Natan Guedes <natan.guedes198@outlook.com>
2019-04-23 22:00:43 -0300

    add files of challange

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Decifrandokinglon.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace Decifrandokinglon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objeto representando o tipo Foo dos kinglons
+            Foo f = new Foo();
+
+            //Objeto representando o alfabeto dos kinglons
+            kinglonAlphabet ka = new kinglonAlphabet();
+
+            KinglonNumbers kn = new KinglonNumbers();
+
+            string textB = System.IO.File.ReadAllText(@"INSIRA O CAMINHO DO SEU ARQUIVO DO TEXT B AQUI");
+            string[] wordsTextB = textB.Split(' ');
+
+            var numberOfPrepositionsTextB = f.howManyPrepositions(wordsTextB);
+
+            Console.WriteLine("O número de preposições do texto B é: " + numberOfPrepositionsTextB);
+
+            var numberOfVerbsTextB = f.howManyVerbs(wordsTextB);
+
+            Console.WriteLine("O número de verbos do texto B é: " + numberOfVerbsTextB);
+
+            var numberOfVerbsTextBinFirstPersob = f.howManyVerbsInFirstPerson(wordsTextB);
+
+            Console.WriteLine("O número de verbos do texto B em primeira pessoa é: " + numberOfVerbsTextBinFirstPersob);
+
+            var numberOfBeatifulNumberInTextB = kn.HoyManyBeatifulNumbers(wordsTextB);
+
+            Console.WriteLine("A quantidade de número bonitos do texto B é: " + numberOfBeatifulNumberInTextB);
+
+            Console.Read();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ kinglonAlphabet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Decifrandokinglon.Classes
+{
+    public class kinglonAlphabet
+    {
+        private string alphabetkinglon = "kbwrqdnfxjmlvhtcgzps";
+
+        //public string OrderByKinglonAkphabet(string[] words)
+        //{
+        //    int alphabetKinglonInInt = Convert.ToInt32(alphabetkinglon);
+        //    List<string> kinglonWordList = words;
+
+        //    foreach (var word in kinglonWordList)
+        //    {
+
+        //    }
+        //}
+    }  
+}
